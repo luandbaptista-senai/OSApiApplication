5478df8833386fb55928884d15ad39365269d276
Luan <luan.d.baptista@aluno.senai.br>
2026-03-23 15:49:25 -0300

    Finalização do Segundo PDF -Criação da base de dados -Configuração de dependências JPA e SQL -Configuração de string de conexão com o banco de dados -Ajuste de Classes -Debug de Queries

--- src/main/java/br/eti/luan/OSApiApplication/api/controller/ClienteController.java
+++ src/main/java/br/eti/luan/OSApiApplication/api/controller/ClienteController.java
@@ -5,6 +5,8 @@
 package br.eti.luan.OSApiApplication.api.controller;
 
 import br.eti.luan.OSApiApplication.domain.model.Cliente;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,14 +18,12 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @RestController
 public class ClienteController {
-    List<Cliente> listaClientes;
+    @PersistenceContext
+            private EntityManager manager;
+    
     @GetMapping("/clientes")
     public List<Cliente> listas() {
-        listaClientes = new ArrayList<Cliente>();
-        listaClientes.add(new Cliente(1, "KGe", "kge@teste.com", "11-99999-9999"));
-        listaClientes.add(new Cliente(1, "Maria", "maria@teste.com", "11-88888-8888"));
-        listaClientes.add(new Cliente(1, "Joao", "joao@teste.com", "11-77777-7777"));
-        return listaClientes;
+        return manager.createQuery("from Cliente", Cliente.class).getResultList();
                 }
     
 }
--- src/main/java/br/eti/luan/OSApiApplication/domain/model/Cliente.java
+++ src/main/java/br/eti/luan/OSApiApplication/domain/model/Cliente.java
@@ -4,14 +4,25 @@
  */
 package br.eti.luan.OSApiApplication.domain.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 /**
  *
  * @author digma
  */
+@Entity
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nome;
     private String email;
+    
+    @Column(name = "telefone")
     private String fone;
 
     @Override
